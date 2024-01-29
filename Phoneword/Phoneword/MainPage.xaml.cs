24670e2cc8827548a8da6cab94698fcf114bebc2
Omer Faruk Arslan 00837088 <o.farukarslan0@gmail.com>
2024-01-29 21:06:25 +0300

    Add phone number translator app

---
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        public const double MyFontSize = 20;
+        public MainPage()
+        {
+            InitializeComponent();
+            //The InitializeComponent method in the page constructor reads the XAML description of the page
+            //loads the various controls on that page, and sets their properties.
+            //You only call this method if you define a page using XAML markup. 
+        }
+        string? translatedNumber;
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber!);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+    }
+}
